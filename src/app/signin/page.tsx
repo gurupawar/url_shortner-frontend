aee84a56f6af053aa0dd65cd33d62de5b2872ba5
Gurushesh Pawar <62001513+gurupawar@users.noreply.github.com>
2024-02-14 19:28:32 +0530

    added api

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -18,7 +18,7 @@ const SignIn: React.FC = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/auth/login", {
+      const response = await fetch("https://short-me.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
