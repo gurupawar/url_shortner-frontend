41993e33f0aa40801c381bb1c49001e54e3651bd
Gurushesh Pawar <62001513+gurupawar@users.noreply.github.com>
2024-08-05 19:46:39 +0530

    Update urlValidator.js

--- backend/src/utils/urlValidator.js
+++ backend/src/utils/urlValidator.js
@@ -1,5 +1,4 @@
 function isValidUrl(url) {
-  // Regular expression for a valid URL
   const urlRegex =
     /^(https?|ftp):\/\/(?:(?:[a-z0-9$\-_.+!*'(),;?&=%#]|%[0-9a-f]{2})+@)?(?:[a-z0-9\-]+\.){1,}(?:[a-z]{2,})(?:\/[^\/\s]*)*$/;
   return urlRegex.test(url);
